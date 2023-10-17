7b4a3cb9736a69b29d3536ef32cc12137bda3b10
Roman Hamov <romanhamov25@gmail.com>
2023-10-17 19:21:01 +0300

    add mocked events

---
+++ src/mocks/events.ts
@@ -0,0 +1,26 @@
+export const events = [{
+    title: 'Event 1',
+    description: 'This is event 1',
+    status: 'Published',
+    date: '2020-12-12',
+    location: 'Location 1',
+    image: 'https://via.placeholder.com/150',
+    id: 1
+  }, {
+    title: 'Event 1',
+    description: 'This is event 1',
+    status: 'Published',
+    date: '2020-12-12',
+    location: 'Location 1',
+    image: 'https://via.placeholder.com/150',
+    id: 1
+  }, {
+    title: 'Event 1',
+    description: 'This is event 1',
+    status: 'Published',
+    date: '2020-12-12',
+    location: 'Location 1',
+    image: 'https://via.placeholder.com/150',
+    id: 1
+  }, 
+]
